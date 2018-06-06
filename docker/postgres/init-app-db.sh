9b5d74376c744cb034660c7ba1dc5a2392b457b7
Pamfilos Fokianos <pamfilosf@gmail.com>
2018-06-06 17:39:14 +0200

    global: refactor - compliant with 'invenio-app' cookiecutter
    * closes #757
    
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- docker/postgres/init-app-db.sh
+++ docker/postgres/init-app-db.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2018 CERN.
@@ -5,8 +6,10 @@
 # CERN Analysis Preservation is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-FROM nginx
-COPY nginx.conf /etc/nginx/nginx.conf
-COPY conf.d/* /etc/nginx/conf.d/
-COPY test.key /etc/ssl/private/test.key
-COPY test.crt /etc/ssl/certs/test.crt
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE ROLE cap WITH LOGIN PASSWORD 'cap';
+    ALTER ROLE cap CREATEDB;
+    \du;
+EOSQL
